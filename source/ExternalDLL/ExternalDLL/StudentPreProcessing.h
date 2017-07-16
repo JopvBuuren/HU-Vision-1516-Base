9cf80cf146133fffe72ad7f6225a1bd706198a43
danielklomp <danielklomp@live.nl>
2017-07-16 12:53:39 +0200

    Finished my part

--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.h
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.h
@@ -9,6 +9,7 @@
 #include "Mask.h"
 #include "SumMask.h"
 #include "ImageFactory.h"
+#include "ImageIO.h"
 class StudentPreProcessing : public PreProcessing {
 public:
 	IntensityImage * stepToIntensityImage(const RGBImage &image) const;
@@ -25,20 +26,28 @@ private:
 		2	Laplacian Guassian
 		3	Sobel
 	*/
-	int method = 3;
-
+	int method = 4;
+	int treshhold = 40;
 	// Default sobel masks
-	std::vector<int> sobelMaskHor = {
+	std::vector<int> sobelMaskHor = std::vector<int>{
 		1, 0, -1,
 		2, 0, -2,
-		1, 0, -1
-	};
+		1, 0, -1 };
 	
-	std::vector<int> sobelMaskVer = { 
+	std::vector<int> sobelMaskVer = std::vector<int>{
 		1, 2, 1,
 		0, 0, 0,
 		-1, -2, -1 
 	};
+	std::vector<int> guassianMaskVer = std::vector<int>{
+		1,2, 1,
+		2, 4, 2,
+		1, 2, 1
+	};
+	std::vector<int> highpassMaskVer = std::vector<int>{
+		-1, -1, -1,
+		-1, 8, -1,
+		-1, -1, -1
+	};
 
-	}
 };
